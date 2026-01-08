6d1b6ecb7ff81e4567f2ed419ed166a829c36346
inemieboka96 <inemieboka96@gmail.com>
2026-01-09 00:24:48 +0100

    Declared DOM variables in the JS file & added a favicon

--- mini-projects/random-password-generator/generatePass.js
+++ mini-projects/random-password-generator/generatePass.js
@@ -2,4 +2,22 @@
 
 // DOM Variables
 
-const passwordOutput = document.getElementById("");
+const passwordOutput = document.getElementById("generated-password");
+//Password length
+let passwordLength = Number(document.getElementById("password-length").value);
+// Checkboxes
+const hasUpperCase = document.getElementById("containsUppercase");
+const hasNumbers = document.getElementById("containsNumbers");
+const hasSymbols = document.getElementById("containsSymbols");
+// Button
+const btnGenerate = document.getElementById("btnGenerate");
+
+// All password characters
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+btnGenerate.addEventListener("click",()=>{
+    //TODO: Implement Logic
+});
