ec2a68f3e5ecdc8041399250de19a0bcae1983a5
inemieboka96 <inemieboka96@gmail.com>
2026-01-09 00:45:13 +0100

    Implemented the most basic form of the password generation logic

--- mini-projects/random-password-generator/generatePass.js
+++ mini-projects/random-password-generator/generatePass.js
@@ -1,10 +1,6 @@
 // DOM Variables
-
-// DOM Variables
-
 const passwordOutput = document.getElementById("generated-password");
-//Password length
-let passwordLength = Number(document.getElementById("password-length").value);
+
 // Checkboxes
 const hasUpperCase = document.getElementById("containsUppercase");
 const hasNumbers = document.getElementById("containsNumbers");
@@ -18,6 +14,21 @@ const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?";
 
-btnGenerate.addEventListener("click",()=>{
-    //TODO: Implement Logic
+function randomIndex(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const arr = [...lowercaseLetters]; // Default array
+
+
+btnGenerate.addEventListener("click", () => {
+  //Password length
+  let passwordLength = Number(document.getElementById("password-length").value);
+
+  let generatedPassword = []; // Initialize empty array
+  let lastIndex = arr.length - 1;
+  for (let i = 0; i < passwordLength; i++) {
+    generatedPassword.push(arr[randomIndex(0, lastIndex)]);
+  }
+  passwordOutput.textContent = generatedPassword.join("");
 });
